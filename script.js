c14448fc0b175410c7171c5523d7772e3185b3b4
Taylor Thomas K <taylorthomask@johndeere.com>
2021-11-14 20:03:36 -0600

    working with JS

--- script.js
+++ script.js
@@ -1,51 +1,80 @@
 //console.log("MARS");
-
-
-async function getImage() {
+//on page load fucntion that gets all photos
+// have list of images
+//send list ti HTMLAllCollection
+const selector = document.querySelector("#photo-selector");
+async function getAllPhotos() {
   try {
-    const res = await axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=2nD1sHhCwt8e8nkf7URbS9XbCa4loKkx9rGhrLex');
-
-
-    const list = Object.keys(res.data);
-    console.log(res)
-    console.log(list)
+    const res = await axios.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=2nD1sHhCwt8e8nkf7URbS9XbCa4loKkx9rGhrLex");
+    console.log(res.data);
     setOptionValues(list);
+
+  } catch (err) {
+    alert(err);
   }
 
-  catch (err) {
-    console.log(err);
-  } finally {
-    console.log("done");
-  }
+  // const list = Object.keys(res.data.mars - photos);
 }
 
 
-console.log(words);
-getImage()
-try {
-
-} catch (error) {
-  console.log(err)
-
+getAllPhotos();
+function setOptionValues(photos) {
+  console.log(photos);
 }
-{
-  let request = new Object.keys.res();
-  request.open.getImage = URL('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=2nD1sHhCwt8e8nkf7URbS9XbCa4loKkx9rGhrLex');
-}
+photos.array.forEach(photos); {
+  let option = document.createElement("option");
+  option.value = photos;
+  option.textContent = photo;
+  selector.appendChild(option);
+};
 
-function prefixString(word) {
-  return function (string) {
-    return `${prefix}${string}`
-  }
+// save for later...
+// async function getImage() {
 
-}
-const word = ['FHAZ'];
-let word = word.filter(string.length);
-{
-  if (word.includes("FHAZ")) {
+//   try {
+//     randomBtn.addEventListener("click", getRandomPics);
+//     const randomUrl = new URL('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=2nD1sHhCwt8e8nkf7URbS9XbCa4loKkx9rGhrLex');
+//     const res = await axios.get(RandomPics);
+//     let getImage = res.data.message;
+//     parseImage([img]);
+//     displayImage(img);
 
-    return word;
-  }
+//   } catch (err) {
+//     console.log(err);
+//   } finally {
+//     console.log("done");
+//   }
+// }
+// function parseImage(str) {
+//   let arr = str.split("/");
+//   console.log(arr[7]);
+
+// }
+
+
+// console.log(image);
+// getImage()
+// try {
+
+// } catch (error) {
+//   console.log(err)
+
+// }
+// {
+//   let request = new Object.keys.res();
+//   request.open.getImage = URL('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=2nD1sHhCwt8e8nkf7URbS9XbCa4loKkx9rGhrLex');
+// }
+
+
+
+// }
+// const word = ['FHAZ'];
+// let word = word.filter(string.length);
+// {
+//   if (word.includes("FHAZ")) {
+
+//     return word;
+//   }
   // let Url = (https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=2nD1sHhCwt8e8nkf7URbS9XbCa4loKkx9rGhrLex");
   // fetch(Url).then(response => {
   // return response.json();
@@ -55,7 +84,7 @@ let word = word.filter(string.length);
   // Work with JSON data here
   // console.log(res.data)
   // catch (err){
-  // console.log(err)
+//   // console.log(err)
 
-}
+// }
 
